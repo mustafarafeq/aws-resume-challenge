d04f6ca01894e563050a0dee6f5c223b4f4b4f21
Mustafa Rafeq <mustafarafeq@mustafas-laptop.mynetworksettings.com>
2025-08-03 15:51:15 -0400

    add resume website files

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Example interactivity – dynamically highlight your name on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const name = document.querySelector("h1");
+  name.style.color = "#004aad";
+});
